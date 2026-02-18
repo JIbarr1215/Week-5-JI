666f573875ab5863fcee8fac9b847292ccd05863
John Ibarra <juan.ibarra08@utrgv.edu>
2026-02-18 18:15:50 +0000

    Co-authored-by: Alexi10763 <Alexi10763@users.noreply.github.com>

---
+++ partner1_02-18.py
@@ -0,0 +1,48 @@
+# 1. Print a welcome message
+print("Welcome to Codespace!")
+
+# 2. Create a variable with your name
+name="John"
+
+# 3. Print the name
+print(name)
+
+# 4. Create a variable with your age
+num=22
+
+# 5. Print the age
+print(num)
+# 6. Ask the user for their favorite color
+color=input("What is your favorite color?")
+
+# 7. Print a sentence using the color
+
+print("Wow I didn't know that", color, "was your favorite color! I like that color too")
+
+# 8. Create two number variables
+a=22
+b=20
+
+# 9. Add the numbers
+sum= a+b
+# 10. Print the result
+print("The sum is", sum)
+
+# 11. Check if age is greater than 18
+a+b > 18
+# 12. Print message if true
+if sum>18:
+      print("Nice, the sum is greater than 18!")
+
+# 13. Otherwise print another message
+else:
+       print("Please try again.")
+# 14. Print message if false
+if sum<18:
+       print("Sum is not greater than 18")
+
+
+
+
+# 15. Print a goodbye message
+print("Thanks for using our codespace!")
